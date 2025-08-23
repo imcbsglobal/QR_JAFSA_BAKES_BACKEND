114d217f6ed53c7dc68bec5d75347868e6a1c555
sajith231 <sajiththomas231@gmail.com>
2025-08-23 23:46:23 +0530

    static settings added

--- qr_jafsa_bakes/settings.py
+++ qr_jafsa_bakes/settings.py
@@ -120,7 +120,12 @@ STATICFILES_DIRS = [
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # optional (only if you keep global static/)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
