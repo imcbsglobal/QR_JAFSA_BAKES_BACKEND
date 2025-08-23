f989d698a1c8fc9bf50da82926892945758f6787
sajith231 <sajiththomas231@gmail.com>
2025-08-23 23:51:19 +0530

    static settings added

--- qr_jafsa_bakes/urls.py
+++ qr_jafsa_bakes/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app1.urls')),
+    path('', include('app1.urls')),  # app1ന്റെ urls ചേർത്തത്
 ]
+
+# Static and Media files serve (only in DEBUG = True)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
