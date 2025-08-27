8f8b34e066b3ebf17f3924d20c48ec5925fd22ad
sajith231 <sajiththomas231@gmail.com>
2025-08-27 11:33:29 +0530

    settings.py

--- qr_jafsa_bakes/settings.py
+++ qr_jafsa_bakes/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'qr_jafsa_bakes.urls'
 
 TEMPLATES = [
